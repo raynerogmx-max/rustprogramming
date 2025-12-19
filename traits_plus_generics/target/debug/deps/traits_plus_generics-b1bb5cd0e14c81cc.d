150ec19f3062648a6a9efc2c6131be4e6ab6aba5
raynerogmx-max <raynerog.mx@gmail.com>
2025-12-19 02:28:34 +0000

    Traits + Generics Hw

---
+++ traits_plus_generics/target/debug/deps/traits_plus_generics-b1bb5cd0e14c81cc.d
@@ -0,0 +1,5 @@
+/workspaces/rustprogramming/traits_plus_generics/target/debug/deps/traits_plus_generics-b1bb5cd0e14c81cc.d: src/main.rs
+
+/workspaces/rustprogramming/traits_plus_generics/target/debug/deps/traits_plus_generics-b1bb5cd0e14c81cc: src/main.rs
+
+src/main.rs:
---
+++ traits_plus_generics/target/debug/traits_plus_generics.d
@@ -0,0 +1,1 @@
+/workspaces/rustprogramming/traits_plus_generics/target/debug/traits_plus_generics: /workspaces/rustprogramming/traits_plus_generics/src/main.rs
